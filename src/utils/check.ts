02236de8e1745299a505c051271a039250ca7de2
Yao <yg_yao@live.com>
2018-10-18 13:52:32 +0800

    update: add search bar check (#66)
    * update: add Number.isInteger polyfill
    
    * update: format Number.isInteger
    
    * update: search bar check

--- src/utils/check.ts
+++ src/utils/check.ts
@@ -1,8 +1,14 @@
 import * as web3utils from 'web3-utils'
+import transitions from '@material-ui/core/styles/transitions'
 
-const checkAddress = address => web3utils.isAddress(address)
-// const checkHeight = address => web3utils.isAddress(address)
-// const checkTransaction = address => web3utils.isAddress(address)
+if (!Number.isInteger) {
+  Number.isInteger = function (value) {
+    /* eslint-disable */
+    return typeof value === 'number' && isFinite(value) && Math.floor(value) === value
+    /* eslint-enable */
+  }
+}
+
 const checkDigitsString = number => {
   console.log('checkDigitsString', number)
   let n = number
@@ -12,11 +18,30 @@ const checkDigitsString = number => {
   return Number.isInteger(n)
 }
 
+const checkAddress = address => web3utils.isAddress(address)
+
+const checkHeight = height => {
+  let h = height
+  if (checkDigitsString(h)) {
+    return true
+  }
+  h = `0x${height}`
+  return checkDigitsString(h)
+}
+
+const checkTransaction = transitionHash => {
+  let hx = transitionHash.toString()
+  if (hx.slice(0, 2) !== '0x') {
+    hx = `0x${hx}`
+  }
+  return hx.length === 66 && checkDigitsString(hx)
+}
+
 const check = {
   address: checkAddress,
-  // height: checkHeight,
+  height: checkHeight,
   digits: checkDigitsString,
-  // transaction: checkTransaction
+  transaction: checkTransaction
 }
 
 const errorMessages = {
