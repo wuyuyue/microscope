6d452a9b83f33f6bb539f283d5f62179dfbde303
Yao <yg_yao@live.com>
2018-10-24 15:58:47 +0800

    Ready for release (#88)
    * update: highlight error icon link
    
    * update: add quota price
    
    * update: add not found img
    
    * hide debug accounts in prod env
    
    * update: add block overtime alert (#82)
    
    * update: add block overtime alert
    
    * fix: remove log in headerbar
    
    * fix: fetch block alert label and set interval
    
    * fix: simple program
    
    * fix:  (#81)
    
    * fix: support 0x/X in account
    
    * update: add Version and Economical message
    
    * fix: refactor values check
    
    * Fix input check and style (#87)
    
    * fix: update block page tx list link color and home page list cell height
    
    * fix: fetch account without 0x
    
    * fix: support without 0x in selector input

--- src/containers/TransactionTable/index.tsx
+++ src/containers/TransactionTable/index.tsx
@@ -13,7 +13,7 @@ import { rangeSelectorText } from '../../utils/searchTextGen'
 import toText from '../../utils/toText'
 import { fromNow } from '../../utils/timeFormatter'
 import valueFormatter from '../../utils/valueFormatter'
-import check, {errorMessages} from '../../utils/check'
+import check, { errorMessages } from '../../utils/check'
 
 interface AdvancedSelectors {
   selectorsValue: {
@@ -132,7 +132,7 @@ class TransactionTable extends React.Component<TransactionTableProps, Transactio
       params[key] = value
     })
     if (this.props.match.params.account) {
-      params.account = this.props.match.params.account
+      params.account = check.format0x(this.props.match.params.account)
     }
 
     const selectorsValue = {}
@@ -148,29 +148,30 @@ class TransactionTable extends React.Component<TransactionTableProps, Transactio
     })
   }
 
-  private fetchTransactions = (params: { [index: string]: string | number } = {}) => {
+  private fetchTransactions = (paramsInput: { [index: string]: string | number } = {}) => {
     // NOTICE: async
+    const params = {}
     this.setState(state => ({ loading: state.loading + 1 })) // for get transactions
+    Object.keys(paramsInput).forEach(key => (params[key] = check.format0x(paramsInput[key])))
     return fetchTransactions(paramsFilter(params))
-      .then(({ result }: { result: { transactions: TransactionFromServer[]; count: number } }) =>
-        this.setState(state =>
-          Object.assign({}, state, {
-            loading: state.loading - 1,
-            count: result.count,
-            items: result.transactions.map(tx => ({
-              key: tx.hash,
-              blockNumber: `${+tx.blockNumber}`,
-              hash: tx.hash,
-              from: tx.from,
-              to: toText(tx.to),
-              age: `${fromNow(tx.timestamp)} ago`,
-              value: valueFormatter(+tx.value),
-              gasUsed: `${+tx.gasUsed}`
-            }))
-          })
-        )
-      )
-      .then(() => this.props.setTransactionsCount && this.props.setTransactionsCount(this.state.count))
+      .then(({ result }: { result: { transactions: TransactionFromServer[]; count: number } }) => {
+        if (this.props.setTransactionsCount) this.props.setTransactionsCount(result.count)
+        this.setState(state => ({
+          ...state,
+          loading: state.loading - 1,
+          count: result.count,
+          items: result.transactions.map(tx => ({
+            key: tx.hash,
+            blockNumber: `${+tx.blockNumber}`,
+            hash: tx.hash,
+            from: tx.from,
+            to: toText(tx.to),
+            age: `${fromNow(tx.timestamp)} ago`,
+            value: valueFormatter(+tx.value),
+            gasUsed: `${+tx.gasUsed}`
+          }))
+        }))
+      })
       .catch(err => {
         this.handleError(err)
       })
